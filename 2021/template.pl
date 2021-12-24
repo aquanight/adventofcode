001c42446bab1c462d79d90aca64404cd48f47fa
aquanight <aquanight@gmail.com>
2021-12-24 06:59:36 -0700

    Finding this too useful to not put in the template

--- 2021/template.pl
+++ 2021/template.pl
@@ -4,6 +4,10 @@ use v5.32;
 use warnings FATAL => 'all';
 use experimental qw/signatures refaliasing declared_refs/;
 
+use Carp ();
+
+sub assert { shift or Carp::confess "ASSERT FAIL"; }
+
 while (<>) {
 
 }
