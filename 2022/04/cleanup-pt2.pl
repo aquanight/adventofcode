77e3783d234d7d76229329f1c33cfdbd2f17c6a0
aquanight <aquanight@gmail.com>
2022-12-21 14:31:14 -0700

    Day 4

---
+++ 2022/04/cleanup-pt2.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+use v5.34;
+use warnings FATAL => 'all';
+use experimental qw/signatures refaliasing declared_refs/;
+
+use Carp ();
+
+sub assert { shift or Carp::confess "ASSERT FAIL"; }
+
+my $score = 0;
+
+while (<>) {
+	chomp;
+	my ($lo1, $hi1, $lo2, $hi2);
+	(($lo1, $hi1, $lo2, $hi2) = /^\s*(\d+)\s*-(\d+)\s*,\s*(\d+)\s*-\s*(\d+)\s*$/) or die "Input error";
+	assert $lo1 <= $hi1;
+	assert $lo2 <= $hi2;
+	if ($lo1 == $lo2 || $hi1 == $hi2) {
+		++$score;
+	}
+	elsif ($lo1 < $lo2) {
+		if ($lo2 <= $hi1) {
+			++$score;
+		}
+	}
+	else {
+		if ($hi2 >= $lo1) {
+			++$score;
+		}
+	}
+}
+
+say $score;
