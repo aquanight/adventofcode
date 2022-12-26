0704abdfd492696cacd0098b2a6fd482356c5984
aquanight <aquanight@gmail.com>
2022-12-25 19:57:01 -0700

    Day 20

---
+++ 2022/20/mixing-pt2.pl
@@ -0,0 +1,116 @@
+#!/usr/bin/perl
+
+use v5.34;
+use warnings FATAL => 'all';
+use experimental qw/signatures refaliasing declared_refs/;
+use integer;
+
+use Carp ();
+use List::Util ();
+
+sub assert { shift or Carp::confess "ASSERT FAIL"; }
+
+my @input;
+
+my $zero;
+
+use constant KEY => 811589153;
+
+while (<>) {
+	chomp;
+	push @input, { value => (KEY * $_), prev => (@input - 1), next => (@input + 1) };
+	if ((0 + $_) == 0) { $zero = $input[-1]; }
+}
+
+assert ref($zero);
+
+$input[0]{prev} = $#input;
+$input[-1]{next} = 0;
+
+sub dumpset {
+	for my $ix (0 .. $#input) {
+		printf STDERR "[%d] value = %d, from = %d, to = %d\n", $ix, $input[$ix]->@{qw/value prev next/};
+	}
+	say STDERR "---";
+}
+#dumpset;
+
+sub unlink_node ($ix) {
+	my $prev = $input[$ix]{prev}//die;
+	my $next = $input[$ix]{next}//die;
+	$input[$prev]{next} = $next;
+	$input[$next]{prev} = $prev;
+	$input[$ix]{prev} = undef;
+	$input[$ix]{next} = undef;
+}
+
+sub insert_after ($ix, $where) {
+	defined $input[$ix]{prev} and die;
+	defined $input[$ix]{next} and die;
+	my $next = $input[$where]{next}//die;
+	$input[$ix]{prev} = $where;
+	$input[$ix]{next} = $next;
+	$input[$where]{next} = $ix;
+	$input[$next]{prev} = $ix;
+}
+
+sub insert_before ($ix, $where) {
+	defined $input[$ix]{prev} and die;
+	defined $input[$ix]{next} and die;
+	my $prev = $input[$where]{prev};
+	$input[$ix]{next} = $where;
+	$input[$ix]{prev} = $prev;
+	$input[$where]{prev} = $ix;
+	$input[$prev]{next} = $ix;
+}
+
+sub move ($ix, $ct) {
+	say STDERR "Moving $ix by $ct";
+	if ($ct >= 0) {
+		$ct %= $#input;
+		$ct > 0 or return;
+		my $where = $input[$ix]{next};
+		unlink_node $ix;
+		while ($ct-- > 1) {
+			$where = $input[$where]->{next};
+		}
+		insert_after $ix, $where;
+	}
+	else {
+		$ct = (-$ct) % $#input;
+		$ct > 0 or return;
+		my $where = $input[$ix]{prev};
+		unlink_node $ix;
+		while ($ct-- > 1) {
+			$where = $input[$where]->{prev};
+		}
+		insert_before $ix, $where;
+	}
+	#dumpset;
+}
+
+for (1 .. 10) {
+	for my $ix (0 .. $#input) {
+		my $item = $input[$ix]{value};
+		move($ix, $item);
+	}
+}
+
+sub get($from, $ct) {
+	$ct %= @input;
+	for (1 .. $ct) {
+		$from = $input[$from->{next}];
+	}
+	return $from->{value};
+}
+
+my $first = get($zero, 1000);
+my $second = get($zero, 2000);
+my $third = get($zero, 3000);
+
+say "First $first, Second $second, Third $third";
+
+my $score = $first + $second + $third;
+
+say $score;
+
